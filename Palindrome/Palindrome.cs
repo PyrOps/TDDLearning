13917e90a88eb67bccdd6beb58c6e903d522581f
Pierre-Yves <py.ravoire@pyrops.eu>
2025-05-19 19:18:58 +0200

    4th test : refactor 5th test : red

--- Palindrome/Palindrome.cs
+++ Palindrome/Palindrome.cs
@@ -23,13 +23,18 @@ namespace Palindrome {
       string input = "abcdefghijklmnopqrstuvwxyz";
       Assert.False(Palindrome.IsPalindrome(input));
     }
+
+    [Fact]
+    public void ComplexShortNonPalindromeStringReturnsFalse() {
+      string input = "abcdea";
+      Assert.False(Palindrome.IsPalindrome(input));
+    }
   }
 
   public class Palindrome {
     public static bool IsPalindrome(string input) {
       if (string.IsNullOrEmpty(input)) return false;
-      if (input.Equals("abcdef")) return false;
-      if (input.Equals("abcdefghijklmnopqrstuvwxyz")) return false;
+      if (input[0] != input[^1]) return false;
       return true;
     }
   }

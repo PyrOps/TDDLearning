9062b156d535fb7ac80b935133c6baf128917ba2
Pierre-Yves <py.ravoire@pyrops.eu>
2025-05-19 18:33:22 +0200

    Ajoutez des fichiers projet.

---
+++ Palindrome/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace Palindrome {
+  public class PalindromeTest {
+    [Fact]
+    public void EmptyStringReturnsFalse() {
+      string input = "";
+      Palindrome p = new();
+      Assert.False(p.IsPalindrome(input));
+    }
+  }
+
+  public class Palindrome {
+    public bool IsPalindrome(string input) {
+      throw new NotImplementedException();
+    }
+  }
+}

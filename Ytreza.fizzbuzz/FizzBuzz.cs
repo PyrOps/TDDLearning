7bb7fa1c5648d42e11289289038f5d26a834956a
Pierre-Yves <py.ravoire@pyrops.eu>
2025-06-17 19:10:07 +0200

    Écriture de la liste des tests à implémenter

--- Ytreza.fizzbuzz/FizzBuzz.cs
+++ Ytreza.fizzbuzz/FizzBuzz.cs
@@ -1,4 +1,24 @@
 namespace Ytreza.fizzbuzz {
+  //
+  // Step 1 : Test list
+  // - Covering non fizzbuzz values
+  //    FizzBuzz(0)
+  //    FizzBuzz(1)
+  //    FizzBuzz(2)
+  // - Covering fizz values (multiples of 3)
+  //    FizzBuzz(3)
+  //    FizzBuzz(6)
+  //    FizzBuzz(9)
+  // - Covering buzz values (multiples of 5)
+  //    FizzBuzz(5)
+  //    FizzBuzz(10)
+  //    FizzBuzz(20)
+  // - Covering fizzbuzz values multiples of 15)
+  //    FizzBuzz(15)
+  //    FizzBuzz(30)
+  //    FizzBuzz(45)
+  //
+
   public class FizzBuzz {
     [Fact]
     public void Test1() {
